d17f262bfb38d0a31ee1b56b98c431c75eb0ec9d
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2019-08-16 11:12:29 +0200

    Add a way to connect and disconnect to/from room

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Chat from './Chat'
 class App extends Component {
   state = {
     room: "Dan's React Chat",
+    connected: false,
   }
   render() {
     return (
@@ -14,17 +15,31 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <label htmlFor="room">
-          Room:&nbsp;
-          <input
-            type="text"
-            id={'room'}
-            placeholder={'Enter room...'}
-            value={this.state.room}
-            onChange={e => this.setState({ room: e.target.value })}
-          />
-        </label>
-        <Chat room={this.state.room} />
+        {!this.state.connected && (
+          <React.Fragment>
+            <label htmlFor="room">
+              Room:&nbsp;
+              <input
+                type="text"
+                id={'room'}
+                placeholder={'Enter room...'}
+                value={this.state.room}
+                onChange={e => this.setState({ room: e.target.value })}
+              />
+            </label>
+            <button onClick={() => this.setState({ connected: true })}>
+              Connect
+            </button>
+          </React.Fragment>
+        )}
+        {this.state.connected && (
+          <React.Fragment>
+            <button onClick={() => this.setState({ connected: false })}>
+              Disconnect from "{this.state.room}"
+            </button>
+            <Chat room={this.state.room} />
+          </React.Fragment>
+        )}
       </div>
     )
   }
