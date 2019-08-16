816476fdd5f9f0d63f31bf739d3fd0ef16c20399
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2019-08-16 11:23:46 +0200

    Store each connecting client to its own room

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,22 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
+const qs = require('qs')
 
-const wss = new WebSocket.Server({ port: 3030 });
+const wss = new WebSocket.Server({ port: 3030 })
+const rooms = {}
 
-wss.on('connection', function connection(ws) {
+function addClientToRoom(roomName, client) {
+  if (Array.isArray(rooms[roomName])) {
+    rooms[roomName].push(client)
+  } else {
+    rooms[roomName] = [client]
+  }
+}
+
+wss.on('connection', function connection(ws, req) {
+  // add every connecting client to its own room
+  const roomName = qs.parse(req.url.split('?')[1])
+  addClientToRoom(roomName.room, ws)
+
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
